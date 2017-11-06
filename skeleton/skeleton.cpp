a96800a4ecc005040011ae6d8d102549cb8f9e65
amantley <amantley@googlemail.com>
2017-11-06 14:46:45 -0800

    nov 6

--- skeleton/skeleton.cpp
+++ skeleton/skeleton.cpp
@@ -27,7 +27,7 @@ int main()
 	printf("\ntesting reset of fork\n");
 	printf("\nupstream change after reset Oct 22\n");
 	printf("\nPile on after rejected pull request Oct 22\n");
-	printf("\nbranch change oct 22\n");
+	printf("\nbranch change nov 6\n");
 
 
 
